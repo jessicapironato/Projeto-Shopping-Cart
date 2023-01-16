482b8be9040bc124d921307be1a46f8cc01d80c2
Jéssica <jessicapmaximo@gmail.com>
2023-01-16 17:10:35 -0300

    requisito 1 e 2 iniciados

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,13 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (query) => {
+  const URL_API = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  try {
+    const response = await fetch(URL_API);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    throw new Error('Termo de busca não informado');
+  }
 };
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,14 +5,18 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    const actual = fetchProductsList;
+    const expected = (Function);
+    expect(actual).toBeInstanceOf(expected);    
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+     await fetchProductsList('computador');
+     expect(fetch).toHaveBeenCalled();
   });
 
   it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
+    
 
   });
 
