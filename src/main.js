71951f4535fc9b87c8c45c3f58969830cc18ca06
Jéssica <jessicapmaximo@gmail.com>
2023-01-16 20:42:29 -0300

    requisito 3 ok

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const productSelected = document.getElementsByClassName('products')[0];
+// console.log(productSelected);
+
+const productPrinted = async () => {
+  const productInformation = await fetchProductsList('computador');
+  productInformation.forEach((element) => {
+    productSelected.appendChild(createProductElement(element));
+  });
+};
+productPrinted();
