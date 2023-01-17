408f85e8eb65b61009d518d9769f88131ca550a5
Jéssica <jessicapmaximo@gmail.com>
2023-01-17 11:07:31 -0300

    feito requisito 4

--- src/main.js
+++ src/main.js
@@ -6,10 +6,27 @@ import { createProductElement } from './helpers/shopFunctions';
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const productSelected = document.getElementsByClassName('products')[0];
-// console.log(productSelected);
+
+// requisito 4 >> faz loadingAdd e cria /append o elemento, faz loadingRemove e add as funções na função productPrinted
+
+const loadingAdd = () => {
+  const loading = document.createElement('p');
+  loading.innerHTML = 'carregando...';
+  loading.classList.add('loading');
+  productSelected.appendChild(loading);
+};
+
+const loadingRemove = () => {
+  const textCarregando = document.getElementsByClassName('loading')[0];
+  productSelected.removeChild(textCarregando);
+};
+
+// requisito 3
 
 const productPrinted = async () => {
+  loadingAdd();
   const productInformation = await fetchProductsList('computador');
+  loadingRemove();
   productInformation.forEach((element) => {
     productSelected.appendChild(createProductElement(element));
   });
